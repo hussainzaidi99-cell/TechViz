ebb386d03a7010e2028280a031e3e9f7b1a8b56b
hussainzaidi99-cell <hussainzaidi99@gmail.com>
2026-08-10 01:24:59 +0500

    Update TechVizLogo.tsx

--- techviz-inc/src/components/TechVizLogo.tsx
+++ techviz-inc/src/components/TechVizLogo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import fullLogo from '../assets/images/techviz_full_logo_1786239432451.jpg';
-import tvIconLogo from '../assets/images/techviz_tv_icon_1786239453839.jpg';
+import tvIconLogo from '../assets/images/Gemini_Generated_Image_g18en3g18en3g18e.png';
 
 interface TechVizLogoProps {
   size?: 'sm' | 'md' | 'lg' | 'xl';
